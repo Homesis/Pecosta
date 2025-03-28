1a1116513d2cdb9459970a22030ae9071aed316b
František Jurek <franta.jurek@gmail.com>
2025-03-28 09:20:28 +0100

    Update

--- Pecosta/Program.cs
+++ Pecosta/Program.cs
@@ -5,38 +5,11 @@ using Pecosta;
 System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 
 
-string filePath = "C:\\Users\\Feda\\source\\repos\\Pecosta\\Pecosta\\Files\\priklad2.xml";
+string filePath = "";
 
 IInvoiceProcesor processor = ProcessorFactory.GetProcessor(filePath);
 
 Invoice invoice1 = processor.ProcessInvoice(filePath);
 
 Console.WriteLine(invoice1);
-
-
-filePath = "C:\\Users\\Feda\\source\\repos\\Pecosta\\Pecosta\\Files\\priklad3.txt";
-
-processor = ProcessorFactory.GetProcessor(filePath);
-
-Invoice invoice2 = processor.ProcessInvoice(filePath);
-
-Console.WriteLine(invoice2);
-
-
-filePath = "C:\\Users\\Feda\\source\\repos\\Pecosta\\Pecosta\\Files\\priklad.csv";
-
-processor = ProcessorFactory.GetProcessor(filePath);
-
-Invoice invoice3 = processor.ProcessInvoice(filePath);
-
-Console.WriteLine(invoice3);
-
-
-filePath = "C:\\Users\\Feda\\source\\repos\\Pecosta\\Pecosta\\Files\\priklad1.csv";
-
-processor = ProcessorFactory.GetProcessor(filePath);
-
-Invoice invoice4 = processor.ProcessInvoice(filePath);
-
-Console.WriteLine(invoice4);
 
